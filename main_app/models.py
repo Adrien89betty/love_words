6f5f07c2e26868ae536aaac89f2c34ad6361b262
adrien leveille <adrienleveille.contact@gmail.com>
2024-07-18 17:01:57 +0200

    Creating UserProfile model => in progress.

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from user_account.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    """All the required informations about the urser's partner"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    partner_name = models.CharField(max_length=100)
+    partner_email = models.EmailField(max_length=254)
+    partner_birthday = models.CharField
+    partner_nickname = models.CharField
+    partner_description = models.CharField
+    date_added = models.DateTimeField(auto_now_add=True)
