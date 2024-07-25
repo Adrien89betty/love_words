60fa881a601d0bcaf4ad80498ed9224a4ed5b6ec
adrien leveille <adrienleveille.contact@gmail.com>
2024-07-25 12:04:52 +0200

    Creating LoveMessage model in main_app.models.

--- main_app/models.py
+++ main_app/models.py
@@ -10,3 +10,12 @@ class UserProfile(models.Model):
     partner_nickname = models.CharField(max_length=100)
     partner_description = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
+
+class LoveMessage(models.Model):
+    """Generated message destinated to the user's partner."""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    scheduled_date = models.DateField()
+    sent_date = models.DateField()
+    sent = models.BooleanField(default=False)
+    
